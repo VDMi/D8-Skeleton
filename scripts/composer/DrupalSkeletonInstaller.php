4b65335e97b46fcd758d72cfec1e0b1b169f54da
Jeroen Bobbeldijk <jeroen@vdmi.nl>
2015-10-07 09:13:44 +0200

    Fix UUID

--- scripts/composer/DrupalSkeletonInstaller.php
+++ scripts/composer/DrupalSkeletonInstaller.php
@@ -38,6 +38,8 @@ class DrupalSkeletonInstaller {
       '__PROJECT_MACHINE_NAME__' => $machine_name,
       '__PROJECT_HUMAN_NAME__' => $human_name,
       '__RANDOM_HASH_SALT__' => DrupalSkeletonInstaller::generateRandomString(64),
+      // Replace default UUID by a new one.
+      'ffac9c12-4a7d-4565-a53e-62ca6fcb4e77' => DrupalSkeletonInstaller::UUID(),
     );
 
     unlink('README.md');
@@ -114,5 +116,28 @@ class DrupalSkeletonInstaller {
       $randomString .= $characters[rand(0, $charactersLength - 1)];
     }
     return $randomString;
+  }
+
+  public static function UUID() {
+    return sprintf('%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
+
+      // 32 bits for "time_low"
+      mt_rand(0, 0xffff), mt_rand(0, 0xffff),
+
+      // 16 bits for "time_mid"
+      mt_rand(0, 0xffff),
+
+      // 16 bits for "time_hi_and_version",
+      // four most significant bits holds version number 4
+      mt_rand(0, 0x0fff) | 0x4000,
+
+      // 16 bits, 8 bits for "clk_seq_hi_res",
+      // 8 bits for "clk_seq_low",
+      // two most significant bits holds zero and one for variant DCE1.1
+      mt_rand(0, 0x3fff) | 0x8000,
+
+      // 48 bits for "node"
+      mt_rand(0, 0xffff), mt_rand(0, 0xffff), mt_rand(0, 0xffff)
+    );
   }
 }
