51c01666b74ca373846cae7008af88862c98294a
Omar Idrissi <omar@vdmi.nl>
2015-10-30 13:33:54 +0100

    Added multi-site_builder

--- scripts/multi-site_builder/builder.sh
+++ scripts/multi-site_builder/builder.sh
@@ -4,10 +4,12 @@ for (( i = 0; i < `php -f decode.php sites_count`; i++ ));
 do
     echo "setting up environment ..."
 
+    PWD=`pwd`
+    BASE_PATH=$PWD/../..
     #statements
     machine_name=`php -f decode.php machine_name $i`
-    db_name=`php -f decode.php $i database`
-    db_username=`php -f decode.php $i username`
+	db_name=`php -f decode.php $i database`
+	db_username=`php -f decode.php $i username`
     db_pass=`php -f decode.php $i password`
     db_host=`php -f decode.php $i host`
     db_driver=`php -f decode.php $i driver`
@@ -20,24 +22,24 @@ do
     for domain in $domains;
     do
     echo $domain
-    echo "$site['$domain'] = '$machine_name';">>../../web/sites/sites.php
+    echo "$site['$domain'] = '$machine_name';">>$BASE_PATH/web/sites/sites.php
     done
     unset IFS
 
-    mkdir ../../web/sites/$machine_name
-    cp ../../web/sites/default/default.settings.php ../../web/sites/$machine_name/settings.php
-    mkdir ../../web/sites/$machine_name/files
+    mkdir $BASE_PATH/web/sites/$machine_name
+    cp $BASE_PATH/web/sites/default/default.settings.php $BASE_PATH/web/sites/$machine_name/settings.php
+    mkdir $BASE_PATH/web/sites/$machine_name/files
 
-    mkdir ../../web/sites/$machine_name/cnf
-    cp sites_config.json ../../web/sites/$machine_name/cnf/config.json
+    mkdir $BASE_PATH/web/sites/$machine_name/cnf
+    cp $BASE_PATH/web/sites_config.json $BASE_PATH/web/sites/$machine_name/cnf/config.json
 
-    cd ../../web/sites/$machine_name/files/ && chmod -R 777 .
-    cd ..
+    cd $BASE_PATH/web/sites/$machine_name/files/ && chmod -R 777 .
+    cd $BASE_PATH/web/sites/$machine_name/
 
     dbconnect="$db_username:$db_pass"
     dburl="$db_driver://$dbconnect@$db_host/$db_name"
 
-    ../../../vendor/bin/drush si $profile --db-url=$dburl --site-name=$site_name --account-name=admin --account-pass=adminpass
+    $BASE_PATH/vendor/bin/drush si $profile --db-url=$dburl --site-name=$site_name --account-name=admin --account-pass=adminpass
 
-    cd ../../../scripts/multi-site_builder/
+    cd $BASE_PATH/scripts/multi-site_builder
 done
